f885c666015df2153c8b8b12176c54132baf7654
lintang-b-s <lintangbirdasaputra23@gmail.com>
2024-06-01 10:40:42 +0700

    fix: loki dashboard panel search logs by keyword

--- domain/prometheus.go
+++ domain/prometheus.go
@@ -15,12 +15,11 @@ type Prometheus struct {
 	FromTime               *timestamppb.Timestamp `json:"fromTime"`
 }
 
-
 type PrometheusNotGRPC struct {
 	CurrentTime            time.Time `json:"currentTime"`
-	AllCPUUsage            float32                `json:"all_cpu_usage"`
-	AllMemoryUsage         float32                `json:"all_memory_usage"`
-	AllNetworkIngressUsage float32                `json:"all_network_ingress_usage"`
-	AllNetworkEgressUsage  float32                `json:"all_network_egress_usage"`
+	AllCPUUsage            float32   `json:"all_cpu_usage"`
+	AllMemoryUsage         float32   `json:"all_memory_usage"`
+	AllNetworkIngressUsage float32   `json:"all_network_ingress_usage"`
+	AllNetworkEgressUsage  float32   `json:"all_network_egress_usage"`
 	FromTime               time.Time `json:"fromTime"`
 }
